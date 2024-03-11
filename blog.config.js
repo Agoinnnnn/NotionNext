c64fd63bf3be6f57413ab226cadabe04a81863c5
Agoinnnnn <162782729+Agoinnnnn@users.noreply.github.com>
2024-03-11 15:20:55 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
   // 烟花色彩，感谢 https://github.com/Vixcity 提交的色彩
   FIREWORKS_COLOR: ['255, 20, 97', '24, 255, 146', '90, 135, 255', '251, 243, 140'],
   // 樱花飘落特效
-SAKURA: process.env.NEXT_PUBLIC_SAKURA || true, // 开关
+  SAKURA: process.env.NEXT_PUBLIC_SAKURA || true, // 开关
 
 // 漂浮线段特效
 NEST: process.env.NEXT_PUBLIC_NEST || false, // 开关
